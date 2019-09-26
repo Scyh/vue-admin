c439dc6110ebf9713be875a83937d71e788636c8
Scyh <8219017332qq.com>
2019-09-26 18:52:52 +0800

    '更细'

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,78 @@
 const path = require('path');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 function resolve(dir) {
     return path.resolve(__dirname, dir);
 }
 
-module.exports = {
+const externals = {
+    'vue': 'Vue',
+    'vue-router': 'VueRouter',
+    'vuex': 'Vuex',
+    'element-ui': 'element-ui',
+    'echarts': 'Echarts'
+}
+
+const cdn = {
+    // 开发环境
+    dev: {
+      css: [
+        'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+        'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
+      ],
+      js: []
+    },
+    // 生产环境
+    build: {
+      css: [
+        'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+        'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
+      ],
+      js: [
+        'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
+        'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
+        'https://cdn.jsdelivr.net/npm/vuex@3.0.1/dist/vuex.min.js',
+        // 'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js',
+        'https://unpkg.com/element-ui/lib/index.js',
+        'https://cdn.bootcss.com/echarts/4.3.0-rc.2/echarts-en.common.min.js'
+      ]
+    }
+}
+
+const productionGzipExtensions = ['js', 'css']
+
+const compress = new CompressionWebpackPlugin({
+    filename: info => {
+        return `${info.path}.gz${info.query}`
+    },
+    algorithm: 'gzip', 
+    threshold: 1024,
+    test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+    minRatio: 0.8,
+    deleteOriginalAssets: false
+})
+
+
+  module.exports = {
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            config.externals = externals;
+            config.plugins.push(compress)
+        }
+    },
     chainWebpack: config => {
         config.resolve.alias.set('@', resolve('src')).end();
+
+        // 将cdn添加到htmlWebpackPlugin配置
+        config.plugin('html').tap(args => {
+            if (process.env.NODE_ENV === 'production') {
+                args[0].cdn = cdn.build
+            }
+              if (process.env.NODE_ENV === 'development') {
+                args[0].cdn = cdn.dev
+              }
+            return args
+        })
 
         config.module.rule('svgIcon').test(/\.svg$/).include.add(resolve('src/icons/svgs')).end()
                             // .use('svg-inline-loader').loader('svg-inline-loader').end()
@@ -23,6 +89,7 @@ module.exports = {
         config.module.rule('svg').exclude.add(resolve('src/icons/svgs')).end();
     },
     css: {
+        extract: true,
 		loaderOptions: {
 			sass: {
                 prependData: `

250f0038fce59c11b5f7f91e8893ca847e84737d
Scyh <8219017332qq.com>
2019-09-28 18:06:04 +0800

    日常更新

---
+++ src/views/lock/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="lock">
+        <el-form class="lock-form" label-width="80px">
+            <el-form-item label="锁屏密码">
+                <el-input type="password" v-model="password"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            password: null,
+        }
+    }
+}   
+</script>
+<style lang="scss">
+.lock {
+}
+</style>
